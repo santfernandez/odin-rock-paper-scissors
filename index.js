be76a603a68efd99f4328933461659e6f33c867a
Santiago Fernandez <santfernandez94@gmail.com>
2022-05-19 20:39:36 -0300

    added player input on js file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+let getPlayerChoice =  prompt('Choose rock, paper or scissors. Write it without capital letter');
+
+let playerChoice = '';
+
+if (getPlayerChoice === 'rock') {
+    playerChoice = 'r';
+} else if (getPlayerChoice === 'paper')  {
+    playerChoice = 'p'
+} else if ((getPlayerChoice === 'scissors')) {
+    playerChoice = 's'
+}
+
+
+console.log(playerChoice);
