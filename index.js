1f564c918ef4d9977150e0aed908d33bb64a9a7a
Santiago Fernandez <santfernandez94@gmail.com>
2022-05-19 23:29:10 -0300

    added computer selection with .random() in js file

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+
+const computerPlay = () => {
+    let getRandomNumber = Math.random();
+
+    let computerChoice = ''
+
+    console.log(getRandomNumber);
+
+    if (getRandomNumber < 0.33 ) {
+        computerChoice = 'r'
+    } else if (getRandomNumber >= 0.33 && getRandomNumber < 0.66 ) {
+        computerChoice = 'p'
+    } else if (getRandomNumber >= 0.66) {
+        computerChoice = 's'
+    }
+
+    return computerChoice;
+};
+
+console.log(computerPlay());
+
+
 let getPlayerChoice =  prompt('Choose rock, paper or scissors. Write it without capital letter');
 
 let playerChoice = '';
@@ -11,4 +34,3 @@ if (getPlayerChoice === 'rock') {
 }
 
 
-console.log(playerChoice);
